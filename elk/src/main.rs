cd2dd941b0a30846f280c130845d93fc0797c581
TheDoctor314 <64731940+TheDoctor314@users.noreply.github.com>
2022-10-13 17:44:54 +0530

    Mess with memory protections to exec from memory
    It works but for some reason the `hello` program does not print
    anything.

--- elk/src/main.rs
+++ elk/src/main.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
 
+use region::Protection;
+
 fn main() -> Result<(), Box<dyn Error>> {
     let input_path = std::env::args().nth(1).expect("usage: elk FILE");
     let input = std::fs::read(&input_path)?;
@@ -13,13 +15,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("{file:#?}");
 
     println!("Disassembling {:?}", input_path);
-    let code = &input[0x1000..];
-    let code = &code[..(std::cmp::min(0x25, code.len()))];
-    ndisasm(code)?;
+    let code_ph = file
+        .program_headers
+        .iter()
+        .find(|ph| ph.mem_range().contains(&file.entry_point))
+        .expect("segment with entry point not found");
+    ndisasm(code_ph.data.as_slice(), file.entry_point)?;
 
-    println!("Executing {:?}", input_path);
-    let entry_point = code.as_ptr();
-    println!("Entry point: {:?}", entry_point);
+    println!("Executing {:?} in memory", input_path);
+    let code = &code_ph.data;
+
+    unsafe {
+        region::protect(code.as_ptr(), code.len(), Protection::READ_WRITE_EXECUTE)?;
+    }
+
+    let entry_offset = file.entry_point - code_ph.vaddr;
+    let entry_point = unsafe { code.as_ptr().add(entry_offset.into()) };
+    println!("       code  @ {:?}", code.as_ptr());
+    println!("entry_offset @ {:?}", entry_offset);
+    println!("entry_point  @ {:?}", entry_point);
+
     unsafe {
         jmp(entry_point);
     }
@@ -32,7 +47,7 @@ unsafe fn jmp(addr: *const u8) {
     fn_ptr();
 }
 
-fn ndisasm(code: &[u8]) -> Result<(), Box<dyn Error>> {
+fn ndisasm(code: &[u8], origin: delf::Addr) -> Result<(), Box<dyn Error>> {
     use std::{
         io::Write,
         process::{Command, Stdio},
@@ -41,6 +56,8 @@ fn ndisasm(code: &[u8]) -> Result<(), Box<dyn Error>> {
     let mut child = Command::new("ndisasm")
         .arg("-b")
         .arg("64")
+        .arg("-o")
+        .arg(format!("{}", origin.0))
         .arg("-")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
